e2bd0242290414a1258badfc5509e0b036ccde96
shindeanant <shindeanant1997@gmail.com>
2021-10-18 13:55:56 +0530

    Added: To analyse sad mood of the user

---
+++ main/java/com/bridgelabz/moodanalyserproblem/MoodAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.moodanalyserproblem;
+
+/**
+ * Purpose - “I am in Sad Mood” message Should Return SAD
+ *
+ * 
+ */
+
+public class MoodAnalyser {
+	public static String analyseMood(String message) {
+		return "Sad";
+	}
+
+}
