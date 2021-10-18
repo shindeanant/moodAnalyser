e72424db1fb43673e1b1c234a7a62f77553d162c
shindeanant <shindeanant1997@gmail.com>
2021-10-18 13:57:58 +0530

    Added: Test case for analyse sad mood of the user

---
+++ test/java/com/bridgelabz/moodanalyserproblem/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyserproblem;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+	// Test case to analyze sad mood of the user
+	@Test
+	public void givenMessageWhenProperShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String actualResult = MoodAnalyser.analyseMood("User is Sad");
+		Assert.assertEquals("Sad", actualResult);
+	}
+
+}
