87f79115c28a0122beba62755e8641ae3846e612
shindeanant <shindeanant1997@gmail.com>
2021-10-18 15:18:06 +0530

    Added: I am sad mood message in constuctor return sad

--- main/java/com/bridgelabz/moodanalyserproblem/MoodAnalyser.java
+++ main/java/com/bridgelabz/moodanalyserproblem/MoodAnalyser.java
@@ -7,20 +7,29 @@ package com.bridgelabz.moodanalyserproblem;
  */
 
 public class MoodAnalyser {
-	public static String analyseMood(String message) {
-		if (message.toLowerCase().contains("sad")) {
-			return "Sad";
-		} else if (message.toLowerCase().contains("happy")) {
-			return "Happy";
-		}
-		return null;
+	public String message;
+
+	// Constructor
+	public MoodAnalyser(String message) {
+		this.message = message;
+		analyseMood();
 	}
 
-	public static void main(String[] args) {
-		String mood = MoodAnalyser.analyseMood("User is Happy");
-		System.out.println(mood);
-		mood = MoodAnalyser.analyseMood("User is Sad");
-		System.out.println(mood);
+	// Getter
+	public String getMessage() {
+		return message;
+	}
+
+	// Setter
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (message.toLowerCase().contains("sad")) {
+			return "Sad";
+		}
+		return "Happy";
 	}
 
 }
