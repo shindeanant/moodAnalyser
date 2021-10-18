61045c786b004286b8ce0445b1d475cea562aadd
shindeanant <shindeanant1997@gmail.com>
2021-10-18 15:07:57 +0530

    Added: Ability to analyse and respond happy or sad mood

---
+++ main/java/com/bridgelabz/moodanalyserproblem/MoodAnalyser.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.moodanalyserproblem;
+
+/**
+ * Purpose - “I am in Sad Mood” message Should Return SAD
+ *
+ *
+ */
+
+public class MoodAnalyser {
+	public static String analyseMood(String message) {
+		if (message.toLowerCase().contains("sad")) {
+			return "Sad";
+		} else if (message.toLowerCase().contains("happy")) {
+			return "Happy";
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		String mood = MoodAnalyser.analyseMood("User is Happy");
+		System.out.println(mood);
+		mood = MoodAnalyser.analyseMood("User is Sad");
+		System.out.println(mood);
+	}
+
+}
---
+++ test/java/com/bridgelabz/moodanalyserproblem/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyserproblem;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		Assert.assertTrue(true);
+	}
+}
